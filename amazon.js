15ae82f62f05479745ae0dea1ad8701b0204ff99
asraf114 <97399247+asraf114@users.noreply.github.com>
2022-04-21 18:01:01 +0300

    starting from scratch right now

---
+++ amazon.js
@@ -0,0 +1,9 @@
+// let anchors = document.getElementsByName('a')
+
+// for (const anchor of anchors){
+// anchor.addEventListener("mouseover", function (event) {
+
+function anchorIntrest(x){
+x.style.color ="orange";
+ }
+        
